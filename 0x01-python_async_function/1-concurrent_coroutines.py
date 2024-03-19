87da66c038b203304840f970cc04a22660fd5853
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-03-19 15:39:14 +0300

    task 2 completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,12 +5,13 @@ In this module we will see about asyncio
 """
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ In this module we will return the list of delay time
 
     Attribs:
